9e092c3552f6be5c2ced12400c20c07b6c7009a5
Prasad Rajesh Posture <95558446+prasadposture@users.noreply.github.com>
2023-03-02 08:42:05 +0530

    Update FDP.py

--- FDP.py
+++ FDP.py
@@ -76,12 +76,6 @@ with st.container():
         st.write('By Prasad Posture')
 
 st.write("___")
-st.write('My Profiles')
-with st.container():
-    left, middle, right = st.columns(3)
-    with left:
-        st.write('[Kaggle](https://www.kaggle.com/prasadposture121)')
-    with middle:
-        st.write('[GitHub](https://github.com/prasadposture)')
-    with right:
-        st.write('[LinkedIn](https://www.linkedin.com/in/prasad-posture-6a3a77215/)')
+<a href="https://www.linkedin.com/in/prasad-posture-6a3a77215/" target="blank"><img align="center" src="https://img.shields.io/badge/-Prasad Posture-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/prasad-posture-6a3a77215/" alt="Prasad Posture" height="20" width="100" /></a>
+<a href="https://github.com/prasadposture" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture-black?style=flat-square&logo=GitHub&logoColor=white&link=https://github.com/prasadposture" alt="prasadposture" height="20" width="100" /></a>
+<a href="https://www.kaggle.com/prasadposture121" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture121-blue?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/prasadposture121" alt="prasadposture121" height="20" width="100" /></a>
