4ddf8d79c1de2e708e948e0e48ed1bb9a8124533
Prasad Rajesh Posture <95558446+prasadposture@users.noreply.github.com>
2023-03-02 08:44:31 +0530

    Update FDP.py

--- FDP.py
+++ FDP.py
@@ -76,6 +76,6 @@ with st.container():
         st.write('By Prasad Posture')
 
 st.write("___")
-<a href="https://www.linkedin.com/in/prasad-posture-6a3a77215/" target="blank"><img align="center" src="https://img.shields.io/badge/-Prasad Posture-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/prasad-posture-6a3a77215/" alt="Prasad Posture" height="20" width="100" /></a>
-<a href="https://github.com/prasadposture" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture-black?style=flat-square&logo=GitHub&logoColor=white&link=https://github.com/prasadposture" alt="prasadposture" height="20" width="100" /></a>
-<a href="https://www.kaggle.com/prasadposture121" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture121-blue?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/prasadposture121" alt="prasadposture121" height="20" width="100" /></a>
+st.markdown("<a href="https://www.linkedin.com/in/prasad-posture-6a3a77215/" target="blank"><img align="center" src="https://img.shields.io/badge/-Prasad Posture-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/prasad-posture-6a3a77215/" alt="Prasad Posture" height="20" width="100" /></a>", unsafe_allow_html=True)
+st.markdown("<a href="https://github.com/prasadposture" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture-black?style=flat-square&logo=GitHub&logoColor=white&link=https://github.com/prasadposture" alt="prasadposture" height="20" width="100" /></a>", unsafe_allow_html=True)
+st.markdown("<a href="https://www.kaggle.com/prasadposture121" target="blank"><img align="center" src="https://img.shields.io/badge/-prasadposture121-blue?style=flat-square&logo=Kaggle&logoColor=white&link=https://www.kaggle.com/prasadposture121" alt="prasadposture121" height="20" width="100" /></a>", unsafe_allow_html=True)
